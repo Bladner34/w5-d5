4cf4e233f4cf1e932fd1d7397e33c95c9aacbbe0
BLadner34 <brittnee.hallman@yahoo.com>
2026-03-08 19:56:41 -0500

    added two pages linked js and began notes

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'; 
+
+//console.log("hello"); <1. Always check it is linked correctly and working. 
+//2. Helper functoin example (for strings)
+function buildEmail(firstName, lastName){
+    
+}
