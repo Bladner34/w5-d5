78bec97e9a0b781c24e8c74dedbf8aaee9a875dc
BLadner34 <brittnee.hallman@yahoo.com>
2026-03-08 22:34:56 -0500

    adding async function and more notes

--- notes.js
+++ notes.js
@@ -1,11 +1,40 @@
 'use strict',
 
 //console.log("hello");<1. Always check it is linked correctly and working.
-//2. Helper functoin example (for strings)
+//2. Helper function example (for strings)
 function buildEmail(firstName, lastName) {
-  const domain = "@gmail.com";
-  const userName = firstName.charAt(0) + lastName;
-  return userName + domain;
+    const firstNameLower = firstName.toLowerCase()
+    const lastNameLower = lastName.toLowerCase()
+    const domain = "@gmail.com";
+    const userName = firstName.charAt(0) + lastName;
+    return userName + domain;
   console.log(firstName.charAt(0) + lastName);
 }
-console.log(buildEmail("Brittnee", "Ladner"));
+//function to GET information from a source
+
+async function getData(url, options){
+    try {
+        const res = await fetch(url, options)
+        if(!res.ok){
+            throw new Error("Error getting data")
+        }
+        const data = await res.json()
+        return data 
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+//main function to run  them all
+
+async function main(){
+    try {
+        const email = buildEmail("Brittnee", "Ladner")
+        console.log(email);
+        const letter = await getData()
+        console.log(letter)
+    } catch (error) {
+        console.error(error)
+    }
+}
+main()
